266e809b0900ae74c7f08ce493b860aeb3a35171
mohit4289 <srttiwari4289@gmail.com>
2026-09-01 21:47:32 +0530

    graceful shutdown added

--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"context"
 	"gin-quickstart/database"
 	"gin-quickstart/handler"
 	"gin-quickstart/repository"
 	"gin-quickstart/router"
 	"gin-quickstart/service"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -17,6 +23,7 @@ func main() {
 	if err != nil {
 		log.Fatal("failed to load .env:", err)
 	}
+
 	r := gin.Default()
 
 	db, err := database.Connect()
@@ -25,10 +32,41 @@ func main() {
 	}
 
 	defer db.Close()
+
 	userRepo := repository.NewUserRepo(db)
 	userService := service.NewUserService(userRepo)
 	userData := handler.GetUserRepo(userRepo)
 
 	router.SetupRoutes(r, userService, userData)
-	r.Run()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: r,
+	}
+	go func() {
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Printf("Server error: %v", err)
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+
+	signal.Notify(
+		quit,
+		os.Interrupt,
+		syscall.SIGTERM,
+	)
+
+	<-quit
+
+	ctx, cancel := context.WithTimeout(
+		context.Background(),
+		5*time.Second,
+	)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Printf("Server shutdown error: %v", err)
+	}
 }
